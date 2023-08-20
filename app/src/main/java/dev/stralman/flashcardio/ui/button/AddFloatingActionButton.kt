1ca8d14752c79c0225e0a71418224fb2a85a8ba1
kjeller <kallestralman@gmail.com>
2023-08-20 21:33:22 +0200

    Fix move FAB on keyboard window inset

--- app/src/main/java/dev/stralman/flashcardio/ui/button/AddFloatingActionButton.kt
+++ app/src/main/java/dev/stralman/flashcardio/ui/button/AddFloatingActionButton.kt
@@ -1,11 +1,18 @@
 package dev.stralman.flashcardio.ui.button
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import dev.stralman.flashcardio.ui.theme.AppTheme
 import dev.stralman.flashcardio.ui.util.ThemePreview
 
@@ -16,7 +23,7 @@ fun AddFloatingActionButton(
     iconContentDescription: String = "",
 ) {
     FloatingActionButton(
-        modifier = modifier,
+        modifier = modifier.imePadding(),
         onClick = onClick,
         content = {
             Icon(
